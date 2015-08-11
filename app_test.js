4edce09d53c2a03b06b49ae946ed0c9d62ddfe3a
pineoc <pineoc@naver.com>
2015-08-12 00:41:56 +0900

    session secret reset

--- app_test.js
+++ app_test.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret: 'keyboard cat',
+  secret: 'coding-project',
   resave: false,
   saveUninitialized: true
 }));
