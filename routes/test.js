7cd767d51bdcb5a0c773c5af75c86f961c2ebed5
Joung Daun <i_co1022@naver.com>
2015-08-23 23:55:29 +0900

    Add 'jQuery', 'Bootstrap' libraries

--- routes/test.js
+++ routes/test.js
@@ -705,4 +705,41 @@ router.get('/web/board_test',function(req,res){
     res.render('test_board',{datas : data});
 });
 
+/*
+ * recommend menu
+ * type : get
+ * req :
+ * res :
+ *
+ * Daun Joung
+ */
+router.get('/web/recommend_menu', function(req, res){
+    res.render('recommend_menu');
+});
+
+/*
+ * fitting_room_list
+ * type : get
+ * req :
+ * res :
+ *
+ * Daun Joung
+ */
+router.get('/web/fitting_room_list', function(req, res){
+    res.render('fitting_room_list');
+});
+
+/*
+ * hot_fashion_list
+ * type : get
+ * req :
+ * res :
+ *
+ * Daun Joung
+ */
+router.get('/web/hot_fashion_list', function(req, res){
+    res.render('hot_fashion_list');
+});
+
+
 module.exports = router;
