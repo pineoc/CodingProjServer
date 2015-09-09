e278db579ca5c57c7e3b6d72d484c17b26a59648
Joung Daun <i_co1022@naver.com>
2015-09-09 15:00:29 +0900

    cloth upload function

--- routes/fileUploadService.js
+++ routes/fileUploadService.js
@@ -31,10 +31,10 @@ var imgFile_check = function(filename){
 
 
 /*
-* file upload file one
-* name : file dir name
-* file : file
-* */
+ * file upload file one
+ * name : file dir name
+ * file : file
+ * */
 exports.fileUpload = function(name,file){
     var resData = {};
     if(typeof file === 'undefined' || file == null){
@@ -82,6 +82,59 @@ exports.fileUpload = function(name,file){
     });
 
     resData.path = urlpath_base+'/'+name+'/'+hashedFilename;
+    resData.result = true;
+    return resData;
+};
+
+/*
+ * file upload file one
+ * name : file dir name
+ * file : file
+ * */
+exports.fileClothUpload = function(name,file,savefilename){
+    var resData = {};
+    if(typeof file === 'undefined' || file == null){
+        console.log('fileService, no file');
+        resData.path = urlpath_base+'/'+'null.png';
+        resData.result = false;
+        return resData;
+    }
+    var filename = file.name;
+    var folder = path.resolve(mypath,name);
+    var srcpath = file.path;
+    var destpath;
+
+
+    if(imgFile_check(filename)==false){
+        console.log('file ext invalid, ext : ',path.extname(filename));
+        resData.path = null;
+        resData.result = false;
+        return resData;
+    }
+
+    if(!fs.existsSync(folder)){
+        mkdirp(folder,function(err){
+            if(err){
+                console.log('error mkdirp, ',err);
+                resData.path = null;
+                resData.result = false;
+                return resData;
+            }
+            else
+                console.log('mkdirp success');
+        });
+    }
+
+    destpath = path.resolve(folder,savefilename+path.extname(filename));
+    var is = fs.createReadStream(srcpath); //소스로부터 스트림을 입력받음
+    var os = fs.createWriteStream(destpath); //읽어온 스트림을 통해서 사진파일 생성
+
+    is.pipe(os);
+    is.on('end',function(){
+        fs.unlinkSync(srcpath);
+    });
+
+    resData.path = urlpath_base+'/'+name+'/'+savefilename+path.extname(filename);
     resData.result = true;
     return resData;
 };
